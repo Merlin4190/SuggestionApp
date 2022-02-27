b7eb563801747e3a1b3a5875e242e23d3708f3a9
Bolanle Ayodeji <abideklove@gmail.com>
2022-02-27 15:07:09 +0100

    Add project files.

---
+++ SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace SuggestionAppLibrary.DataAccess
+{
+    public class DbConnection
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IMongoDatabase _db;
+        private string _connectionId = "MongoDB";
+
+        public string DbName { get; private set; }
+        public string CategoryCollectionName { get; private set; } = "categories";
+        public string StatusCollectionName { get; private set; } = "statuses";
+        public string UserCollectionName { get; private set; } = "users";
+        public string SuggestionCollectionName { get; private set; } = "suggestions";
+
+        public MongoClient Client { get; private set; }
+        public IMongoCollection<Category> MyProperty { get; private set; }
+
+        public DbConnection(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
---
+++ SuggestionAppLibrary/Models/SuggestionModel.cs
@@ -0,0 +1,20 @@
+﻿namespace SuggestionAppLibrary.Models
+{
+    public class SuggestionModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Suggestion { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+        public Category Category { get; set; }
+        public BasicUserModel Author { get; set; }
+        public HashSet<string> UserVotes { get; set; } = new();
+        public Status SuggestionStatus { get; set; }
+        public string OwnerNotes { get; set; }
+        public bool ApproveForRelease { get; set; } = false;
+        public bool Archived { get; set; } = false;
+        public bool Rejected { get; set; } = false;
+    }
+}
